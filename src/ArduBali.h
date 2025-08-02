3130749387f2d6b9bd8f01e443afa9f265388868
I Gede Narayana Parameswara <116192560+Npswara@users.noreply.github.com>
2025-08-02 17:00:44 +0800

    Update ArduBali.h

--- src/ArduBali.h
+++ src/ArduBali.h
@@ -3,25 +3,29 @@
 
 #include <Arduino.h>
 
-// Konstanta untuk status perangkat
+
 #define PESU OUTPUT
 #define MECELEP INPUT
 #define NYALE HIGH
 #define AMOR LOW
+#define angkaDesimal float
+#define becik true
+#define salah false
 
-// Makro untuk menggantikan fungsi pinMode, digitalWrite, dan delay
 #define ngaturang pinMode
 #define kontrol digitalWrite
 #define antos delay
+#define maca digitalRead 
 
-
-// Makro untuk fungsi serial
-#define Mekawit Serial.begin
+#define Mekawit.begin Serial.begin
 #define cetak Serial.print
 #define cetakln Serial.println
+#define ngirim analogWrite 
+#define macaAnalog analogRead 
+#define antuk for
 
-// Makro untuk menggantikan tipe data dan fungsi utama
 #define angke int
+#define angkeDesimal float
 #define fungsi void
 #define nyiapang() setup()
 #define abadi() loop()
