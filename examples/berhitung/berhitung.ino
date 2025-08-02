f1f6b158df689b4b9a6e538bb357513acf8d9019
I Gede Narayana Parameswara <116192560+Npswara@users.noreply.github.com>
2025-08-02 18:00:08 +0800

    Update berhitung.ino

--- examples/berhitung/berhitung.ino
+++ examples/berhitung/berhitung.ino
@@ -3,15 +3,14 @@
 angke i; // Tipe data angka (int)
 
 fungsi nyiapang() {
-    Mekawit(9600)
-    antuk(i = 0; i < 10; i = i = 1){
-        cetak("Angke ke - ");
-        cetak(i);
-        cetakln(""); 
-        antos(500)
+  mekawit(9600);
+    antuk(i = 0; i < 11; i++){
+      cetak("Angke ke - ");
+      cetak(i);
+      cetakln(""); 
+      antos(1000);
     }
-    cetak("Program suud")
+    cetak("Program suud");
 }
 fungsi abadi() {
 }
-
